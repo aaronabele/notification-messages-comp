873da513eeff0a6bf711619546cb59ab0fa3de4b
Aaron Abele <aaronabele.coding@gmail.com>
2023-03-19 12:34:07 +0100

    adds notification center

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>Notification Center</h1>
+  <NotificationMessage type="error" header="Oh no!">
+    <p>Your flight has been <b>canceled</b></p>
+  </NotificationMessage>
+
+  <NotificationMessage type="success" header="Oh yeah!">
+    <p>Your flight is on time!</p>
+  </NotificationMessage>
+
+  <NotificationMessage type="info" header="Good morning!">
+    <p>HAVE A NICE DAY</p>
+  </NotificationMessage>
+</template>
+
+<script>
+import NotificationMessage from "./components/NotificationMessage.vue";
+
+export default {
+  name: "App",
+  components: {
+    NotificationMessage,
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/NotificationMessage.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app" class="ui container">
+    <!-- Notification Message Markup -->
+    <!-- Types: info, warning, error, success -->
+    <div class="ui message" :class="type" v-if="!hidden">
+      <i class="close icon" @click="hide"></i>
+      <div class="header">{{ header }}</div>
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotificationMessage",
+  props: {
+    type: String,
+    header: String,
+  },
+  data() {
+    return {
+      hidden: false,
+    };
+  },
+  methods: {
+    hide() {
+      this.hidden = true;
+    },
+  },
+};
+</script>
+
+<style scoped>
+#app {
+  margin-top: 1rem;
+}
+</style>
